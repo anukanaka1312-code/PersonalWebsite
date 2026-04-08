84246bbe5ce14ef81dd5c4fc96d1f3faa0b730f7
Annapoorneshwari K G <anu@Annapoorneshwaris-MacBook-Air.local>
2026-04-08 23:45:10 +0530

    chore: add nginx configuration and deployment setup
    - Add simple nginx config for anukanaka.in domain
    - Update ALLOWED_HOSTS with domain and www subdomain
    - Add deployment setup documentation
    - Include DNS configuration instructions

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_0*r8%viu__^zwoumpu4+81=m@a1#%zmae6540)xggd8e*#01u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anukanaka.in', 'www.anukanaka.in', 'localhost', '127.0.0.1']
 
 
 # Application definition
